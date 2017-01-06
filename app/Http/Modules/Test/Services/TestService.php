a3ac4b4a21899abd528dd5eed02d5da1433f32d7
lxlxw <19168066@qq.com>
2017-01-06 09:46:52 +0800

    test redisservice

--- app/Http/Modules/Test/Services/TestService.php
+++ app/Http/Modules/Test/Services/TestService.php
@@ -5,9 +5,13 @@
 namespace App\Http\Modules\Test\Services;
 
 use App\Http\Core\Service;
+use App\Http\Common\RedisOp;
 
 class TestService extends Service
 {
+    
+    protected $_redis;
+    
     /**
      * test 
      * @param array $p_arrParam
@@ -15,6 +19,15 @@ class TestService extends Service
      */
     public function test($p_arrParam)
     {
+        $this->_redis = RedisOp::getInstance('default');
+        
+        $ret = $this->_redis->get($p_arrParam['id']);
+        if ($ret !== false) {
+            return ['name' => $ret];
+        }
+        
+        //TODO: db class
+        
         if($p_arrParam['id'] != 1){
             $this->arrError = ['errno' => 1011, 'errmsg'=> 'username not exist.'];
             return false;
